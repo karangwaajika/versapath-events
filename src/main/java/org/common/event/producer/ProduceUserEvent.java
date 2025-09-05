939e2e90fe06eb4563424f062dd2645cd07471e5
kevin <murindakevinmulk@gmail.com>
2025-09-05 11:58:09 +0200

    chores: Change the deployment version

--- src/main/java/org/common/event/producer/ProduceUserEvent.java
+++ src/main/java/org/common/event/producer/ProduceUserEvent.java
@@ -1,17 +1,20 @@
-package org.common.event;
+package org.common.event.producer;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class CreateUserEvent {
-    private Long id;
+    private UUID versapathUserId;
     private String email;
     private String lastName;
     private String firstName;
+    private String username;
 }
