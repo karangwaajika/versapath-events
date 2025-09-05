26535603d6ec17b02a1b9196e90be4ebc1d301ed
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-05 09:50:02 +0200

    create test event

--- src/main/java/org/common/Main.java
+++ src/main/java/org/common/Main.java
@@ -1,4 +1,4 @@
-package org.events;
+package org.common;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/org/common/event/CreateUserEvent.java
@@ -0,0 +1,15 @@
+package org.common.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateUserEvent {
+    private Long id;
+    private String email;
+}
