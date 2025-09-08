6fc9095606770228ccdfe95275b3887a5f4d6e63
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-08 11:51:50 +0200

    update

--- src/main/java/org/common/event/TestEvent.java
+++ src/main/java/org/common/event/TestEvent.java
@@ -1,4 +1,17 @@
 package org.common.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestEvent {
+    String name;
+    String location;
+    int age;
+    int year;
 }
