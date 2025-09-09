9571ca36fd125796916e307f1ffc24e578e8f5d5
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-09 21:25:17 +0200

    add skill taxonomy event classes

---
+++ src/main/java/org/common/event/GrowthTrackEvent.java
@@ -0,0 +1,21 @@
+package org.common.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GrowthTrackEvent {
+    UUID id;
+    String name;
+    private String description;
+    List<Map<UUID, Integer>> skillCapsules; // capsule ID with its sequence order
+}
---
+++ src/main/java/org/common/event/SkillCapsuleEvent.java
@@ -0,0 +1,24 @@
+package org.common.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SkillCapsuleEvent {
+    UUID id;
+    String name;
+    private int moodleCourseId;
+    private String description;
+    private String difficulty;
+    String proficiencyLevel;
+    List<Map<UUID, Integer>> skillAtom; // atom ID with its sequence order
+}
