7e75b469e566c4e640acf2a9168d22078373facd
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-19 13:05:15 +0200

    add new class

---
+++ src/main/java/org/common/event/AssignAtomToCapsuleEvent.java
@@ -0,0 +1,19 @@
+package org.common.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssignAtomToCapsuleEvent {
+    private int moodleCourseId;
+    private List<Map<UUID, Integer>> skillAtom; // atom ID with its sequence order
+}
