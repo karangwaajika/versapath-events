500132c00732bb8b3cbc076b86361f1a3716b338
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-24 09:58:32 +0200

    feat: update assignAtom

--- src/main/java/org/common/event/AssignAtomToCapsuleEvent.java
+++ src/main/java/org/common/event/AssignAtomToCapsuleEvent.java
@@ -6,8 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 @Data
 @Builder
@@ -15,5 +13,6 @@ import java.util.UUID;
 @AllArgsConstructor
 public class AssignAtomToCapsuleEvent {
     private int moodleCourseId;
+    private String courseName;
     private List<String> atoms;
 }
