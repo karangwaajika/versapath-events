856cb6ae5e897d0794d9a2f6de3752b73dfbdb3f
kevin <murindakevinmulk@gmail.com>
2025-09-26 16:19:15 +0200

    feat: add ProduceMentorEvent

---
+++ src/main/java/org/common/event/ProduceMentorEvent.java
@@ -0,0 +1,22 @@
+package org.common.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProduceMentorEvent {
+    private UUID versapathUserId;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private List<UUID> specializations;
+}
