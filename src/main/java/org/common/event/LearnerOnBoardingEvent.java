b053f86859e355c3d0d7f5edf30f496467afbd2b
kevin <murindakevinmulk@gmail.com>
2025-09-29 14:50:07 +0200

    feat: create new event class LearnerOnBoardingEvent

---
+++ src/main/java/org/common/event/LearnerOnBoardingEvent.java
@@ -0,0 +1,17 @@
+package org.common.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LearnerOnBoardingEvent {
+    private UUID learnerId;
+    private boolean requiresOnboarding;
+}
