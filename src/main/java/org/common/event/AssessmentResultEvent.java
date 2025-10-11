116cc6bc58d2e831aa0804a73c97f03e694346b3
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-10-11 09:48:22 +0200

    add assessment result event

---
+++ src/main/java/org/common/event/AssessmentResultEvent.java
@@ -0,0 +1,25 @@
+package org.common.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AssessmentResultEvent {
+    private UUID userId;
+    private UUID assessmentId;
+    private String assessmentName;
+    private int moodleQuizId;
+    private double grade;
+    private LocalDateTime timeStart;
+    private LocalDateTime timeFinish;
+    private int attemptNumber;
+
+}
